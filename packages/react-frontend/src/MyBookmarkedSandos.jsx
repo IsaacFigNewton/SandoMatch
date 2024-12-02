6c21c3c7fe94a4680353c9bc8af10cba34fffe6e
nlarter <nicodlarter73@gmail.com>
2024-12-02 15:43:19 -0800

    new error fixes

--- packages/react-frontend/src/MyBookmarkedSandos.jsx
+++ packages/react-frontend/src/MyBookmarkedSandos.jsx
@@ -1,6 +1,6 @@
 //MyBookmarkedSandos.jsx
 //import React from "react";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 function MyBookmarkedSandos() {
